f6a93d709d187289f7cd2daec39811f9485c94ca
Fardin Rahman <47810892+NeutronSlayer@users.noreply.github.com>
2023-01-03 22:15:32 +0600

    Update chaingLiner.cpp

--- chaingLiner.cpp
+++ chaingLiner.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
 using namespace std;
+void insert(int arr[], int x)
+{
+    int k = 0;
+    int mod = 0, primod = 0;
+    bool found = false;
+    while (!found)
+    {
+        if (k == 0)
+        {
+            mod = x % 10;
+            primod = mod;
+        }
+        else
+        {
+            mod = (primod + k) % 10;
+        }
+
+        if (arr[mod] == 0)
+        {
+
+            found = true;
+            arr[mod] = x;
+        }
+        k++;
+    }
+}
+
+void printHashTable(int array[], int hashtable[])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << "--> " << hashtable[i] << endl;
+    }
+}
 
 int main()
 {
     int array[10] = {1, 4, 6, 7, 8, 9, 10, 22, 23, 24};
     int hashtable[10] = {};
     int mod;
-    int i = 0, k =0;
-    while (i <10)
-    {
-        //cout<< i<<"--> "<<k;
-        mod = (k+array[i]) % 10;
-        //cout<<"--> "<<mod<<"--> "<<array[i]<<endl;
-        if (hashtable[mod] == 0)
-        {
-            //cout<<"!!!!!!"<< hashtable[mod] <<"-->"<< array[i]<<endl;
-            hashtable[mod] = array[i];
-            i++;
-            k = 0;
-        }
-        k++;
-    }
+    int i = 0, k = 0, priMod;
 
     for (int i = 0; i < 10; i++)
     {
-        cout<<array[i]<<" ";
+        insert(hashtable, array[i]);
     }
-    cout<<endl;
 
-    for (int i = 0; i < 10; i++)
-    {
-        cout<< i<<"--> "<<hashtable[i]<<endl;
-    }
+    printHashTable(array, hashtable);
 }
